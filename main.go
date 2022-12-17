77a8305cc3c9ae51e0f2d99be5d7e3d7b943cb3b
Your Name <you@example.com>
2022-12-17 13:48:28 +0700

    update add router

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package myLogger
 
-import "log"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func InitFunction() {
 	log.Println("initial function from myLogger")
+}
+
+func InitRoute() {
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusAccepted, gin.H{
+			"status":  true,
+			"message": "Transaction success",
+			"detail":  "",
+		})
+	})
+	router.Run(":8088")
 }
